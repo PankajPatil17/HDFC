47c1a88aa14e23da9d2f0028b1ac4905c9ca37cb
Pankaj <patilpankaj1772@gmail.com>
2023-01-23 11:17:36 +0530

    first commit

--- lib/Controller/HomePageController.dart
+++ lib/Controller/HomePageController.dart
@@ -5,21 +5,9 @@ import 'package:hdfc_bank/Controller/SignUpController.dart';
 import 'package:hdfc_bank/Generic/Constant/variables.dart';
 
 class HomePageController extends GetxController {
-  List networkhospitalimages = [].obs;
-  List activepackages = [].obs;
-  List teleconsultation = [].obs;
-  List symptoms = [].obs;
-  List cityList = [];
-  var videos;
-  List packagesListDetails = [].obs;
-  List packagesList = [].obs;
-  List CheckPackageaval = [];
-  var callFunctionmessage;
   var SignUpcontroller = Get.put(SignUpController());
   var notificationCount;
-  List blogs = [].obs;
-  List blogsdetail = [].obs;
-  List onboardingimages = [].obs;
+
   var Usermobilenumber;
 
   List BannerImages = [].obs;
@@ -34,4 +22,24 @@ class HomePageController extends GetxController {
     var decodedResponse = json.decode(response.body);
     BannerImages = decodedResponse['Home_slider'];
   }
+
+  List HealthName = [
+    'Policy Details',
+    'My Coverage',
+    'Network Hospital',
+    'Intimate My Claims',
+    'Download Forms',
+    'Members Covered',
+    'My Claim Status',
+  ];
+
+  List HealthImage = [
+    'assets/Images/Policy-details.svg',
+    'assets/Images/My_Coverage.svg',
+    'assets/Images/Network_hospital.svg',
+    'assets/Images/Intimate_my_claim.svg',
+    'assets/Images/Download_form.svg',
+    'assets/Images/Members-Covered.svg',
+    'assets/Images/Claim-Status.svg',
+  ];
 }
